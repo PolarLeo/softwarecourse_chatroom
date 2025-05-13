9b37d76d0be82187e7b7a7188638295232b79997
kshhhv <nikhil.keshav@ltu.se>
2025-05-13 14:49:14 +0200

    first idea

--- host.py
+++ host.py
@@ -1,16 +1,18 @@
-# Echo server program
 import socket
 
-HOST = ""  # Symbolic name meaning all available interfaces
-PORT = 50007  # Arbitrary non-privileged port
+HOST = 'localhost'                 # Symbolic name meaning all available interfaces
+PORT = 50009             # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+
+                print(f' {addr}: {data.decode("utf-8")}')
+
+                conn.sendall(f'Server got the data from {addr}'.encode('utf-8'),  )
